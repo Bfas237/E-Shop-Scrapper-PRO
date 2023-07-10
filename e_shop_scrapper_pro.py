4ebec82367df0ba3f740a2812357994bd5846ded
Ningmua Bruno <bfaschatinc@gmail.com>
2023-07-10 15:06:30 +0100

    Summary of changes made to the code:
    1. Updated the code to handle the case when the description tab is not available. Added a check to determine if the description tab exists and extracted the description content if available. Set the value of `description_content` to `None` if the description tab is not present.
    
    2. Added a similar check for the additional information tab. Extracted the content of the additional information tab if available, and set the value of `additional_info_content` to `None` if the tab is not present.
    
    3. Modified the code for extracting the summary section. Updated the variable name to `summary_element` and used the `decode_contents()` method to extract the contents of the summary section. Set the value of `summary` to `None` if the summary section is not found.
    
    4. Updated the code for joining the tags. Added a check to ensure `tags` is iterable before applying the `join()` method. If `tags` is empty, set the value of "tax:product_tag" to `None` in the dictionary.
    
    These changes improve the handling of missing or unavailable sections and ensure that the extracted values are set appropriately.

--- e_shop_scrapper_pro.py
+++ e_shop_scrapper_pro.py
@@ -24,12 +24,11 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 productlinks = []  # List to store product links
 data = []  # List to store product data
 categories = []  # List to store categories
-base_url = "https://rhapsodybikeparts.com/shop/page/"  # Base URL for the product pages
-
+base_url = "https://rcfminibikes.com/product-category/all/page/"  # Base URL for the product pages
 
 # Send a request to the first page to get the total number of products
 response = requests.get(base_url + "1", headers=headers)
-
+soup = BeautifulSoup(response.text, 'html.parser')
 # Define the number of products displayed per page
 products_per_page = 16 
 
@@ -53,28 +52,33 @@ else:
     # If no pagination information is found, set total_products to 0
     total_products = 0
 
-print(total_products)
-
 # Calculate the total number of pages
 
 total_pages = (total_products - 1) // products_per_page + 1
-
 # Loop through each page
 for page in tqdm(range(1, total_pages + 1), desc='Scraping Pages'):
     url = base_url + str(page)
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find all product summaries on the current page
+    product_list = soup.find_all('div', class_='wpbf-woo-loop-summary')
 
-    # Find all product titles on the current page
-    product_list = soup.find_all("h2", {"class": "woocommerce-loop-product__title"})
-
-    # Extract the product links from the product titles
-    for product in product_list:
-        link = product.find("a", {"class": "woocommerce-LoopProduct-link-title"}).get('href')
+    # Extract the product links from the product summaries
+    for product_summary in product_list:
+        # Extract the post title from the <h3> tag
+        post_title = product_summary.find('h3', class_='woocommerce-loop-product__title').text
+       
+        # Extract the href link from the <a> tag
+        link = product_summary.find('a', class_='woocommerce-LoopProduct-link')['href']
         productlinks.append(link)
 
 # Initialize a list to store product names
 product_names = []
+import csv
+
+# Initialize the list to store product data
+data = []
 
 # Iterate through each product link
 for link in tqdm(productlinks, desc='Scraping Products'):
@@ -83,144 +87,218 @@ for link in tqdm(productlinks, desc='Scraping Products'):
     hun = BeautifulSoup(f, 'html.parser')
 
     try:
-        # Find the <img> tag with class "wp-post-image" within the BeautifulSoup object
-        img_tag = hun.find('img', class_='wp-post-image')
-
-        # Check if the <img> tag is found
-        if img_tag:
-            # Extract the image URL from the 'src' attribute of the <img> tag
-            image_url = img_tag['src']
-
-            # Concatenate the updated image details using the "!" symbol
-            updated_image_details = f"{image_url}"
-            
-            # Print each set of details on a new line
-            image = updated_image_details
-    except:
-        image = None
-
-    try:
-        # Extract the category from the product details
-        category_span = soup.find('span', class_='posted_in')
-        find_category = hun.find("div", {"class": "single-product-category"}).text.replace('\n', "")
-
-        # If the category span exists, extract each category name
-        if category_span:
-            for category_link in category_span.find_all('a'):
-                category_name = category_link.text
-                categories.append(category_name)
-
-        # Join the categories into a string
-        category_result = ', '.join(categories)
-
-        # Use the category from product details if available, otherwise use the categories extracted
-        category = find_category if find_category is not None else category_result
-    except:
-        category = None
-
-    try:
         # Extract the price from the product details
-        price_element = hun.find('span', class_='woocommerce-Price-amount')
-        price = price_element.text.strip()
-    except:
+        price_element = hun.find('p', class_='price')
+
+        # Check if the price element exists and contains a valid price
+        if price_element and price_element.text.strip() != '':
+            price = price_element.find('bdi').text.strip()
+        else:
+            price = None
+
+    except Exception as e:
+        print(f"Error: {e}")
         price = None
 
     try:
-        # Extract the product description from the product details
-        descriptions = hun.find_all('div', attrs={"aria-labelledby": "tab-title-description"})[0]
-        unwrapped_elements = descriptions.find_all(["h2", "p", "ul"])
-        unwrapped_contents = [str(element) for element in unwrapped_elements]
-        result = ' '.join(unwrapped_contents)
-        about = result
-    except:
-        about = None
-
-    try:
         # Extract the product name from the product details
-        name = hun.find("h1", {"class": "product_title"}).text.replace('\n', "")
+        name = hun.find('h1', class_='product_title').text.strip()
         product_names.append(name)
-    except:
+
+    except Exception as e:
+        print(f"Error: {e}")
         name = None
 
     try:
         # Generate the slug from the name
         slug = re.sub(r'\s+', '_', name).lower()
         slug = re.sub(r'[^\w\s-]', '', slug)
-    except:
+
+    except Exception as e:
+        print(f"Error: {e}")
         slug = None
 
     try:
-        # Assign the formatted date and time to the 'post_date' key in the dictionary
-        date = formatted_date
-    except:
-        date = None
+        # Find the summary section
+        summary_element = hun.find('div', class_='woocommerce-product-details__short-description')
+
+        # Extract the contents under the summary section
+        summary = summary_element.decode_contents().strip() if summary_element else None
+
+    except Exception as e:
+        print(f"Error: {e}")
+        summary = None
+
+    try:
+        # Extract the SKU from the product details
+        sku = hun.find('span', class_='sku').text.strip()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sku = None
 
     try:
         # Join the product names using ', ' and assign to the 'tags' variable
-        tags = ', '.join(product_names)
-    except:
+        categories_element = hun.find('span', class_='posted_in')
+        tags = [a.text for a in categories_element.find_all('a')]
+        print("Featured Tags:", ", ".join(tags))
+
+    except Exception as e:
+        print(f"Error: {e}")
         tags = None
+
+
+    description_html = None  # Initialize the variable outside the try-except block
+
+    try:
+        # Find the description tab
+        description_tab = hun.find('li', class_='description_tab')
+        
+        if description_tab:
+            description_content = description_tab.find_next('div', class_='woocommerce-Tabs-panel')
+            
+            if description_content:
+                description_html = description_content.decode_contents()
+            else:
+                description_html = None
+        else:
+            description_html = None
+
+    except Exception as e:
+        print(f"Error: {e}")
+        description_html = None
+
+    try:
+        # Find the additional information tab
+        additional_info_tab = hun.find('li', class_='additional_information_tab')
+        
+        if additional_info_tab:
+            additional_info_content = additional_info_tab.find_next('div', class_='woocommerce-Tabs-panel').decode_contents()
+
+            # Extract the attribute details from additional_info_content
+            additional_info_soup = BeautifulSoup(additional_info_content, 'html.parser')
+            attribute_elements = additional_info_soup.find_all('tr', class_='woocommerce-product-attributes-item')
+
+            attributes = {}
+            for attribute_element in attribute_elements:
+                attribute_label = attribute_element.find('th', class_='woocommerce-product-attributes-item__label').text.strip()
+                attribute_value = attribute_element.find('td', class_='woocommerce-product-attributes-item__value').text.strip()
+
+                attributes[attribute_label] = attribute_value
+
+            attribute = attributes.get("Attribute", "")
+            attribute_data = attributes.get("Attribute Data", "")
+            attribute_default = attributes.get("Attribute Default", "")
+        else:
+            additional_info_content = None
+            attribute = ""
+            attribute_data = ""
+            attribute_default = ""
+
+    except Exception as e:
+        print(f"Error: {e}")
+        additional_info_content = None
+        attribute = ""
+        attribute_data = ""
+        attribute_default = ""
+
+
+    try:
+       
+        image_div = hun.find('div', class_='woocommerce-product-gallery')
+        image_tags = image_div.find_all('img')
+
+        featured_image = image_tags[0]['src'] if image_tags else None
+        gallery_images = [img['src'] for img in image_tags[1:]] if len(image_tags) > 1 else None
+
+        featured_images = f"{featured_image} ! alt : {name} ! title : {name}.jpg ! desc : {name} ! caption : {' | '.join(gallery_images) if gallery_images is not None else ''}"
+        
+        
+
+
+    except Exception as e:
+        print(f"Error: {e}")
+        featured_images = None
+        gallery_images = None
+
+    try:
+        # Find the last <a> tag in the breadcrumb navigation
+        breadcrumb_nav = hun.find('nav', class_='woocommerce-breadcrumb')
+        last_a_tag = breadcrumb_nav.find_all('a')[-1]
+
+        # Get the text of the last <a> tag, which is the category
+        category = last_a_tag.text.strip()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        category = None
 
     # Create a dictionary to store the product data
     product_data = {
-        "post_title": name,
-        "post_name": slug,
-        "post_parent": "",
-        "ID": "",
-        "post_content": about,
-        "post_excerpt": "",
-        "post_status": "publish",
-        "post_password": "",
-        "menu_order": "",
-        "post_date": date,
-        "post_author": "",
-        "comment_status": "open",
-        "sku": "",
-        "parent_sku": "",
-        "children": "",
-        "downloadable": "no",
-        "virtual": "no",
-        "stock": "",
-        "regular_price": price,
-        "sale_price": "",
-        "weight": "",
-        "length": "",
-        "width": "",
-        "height": "",
-        "tax_class": "",
-        "visibility": "",
-        "stock_status": "instock",
-        "backorders": "no",
-        "sold_individually": "no",
-        "low_stock_amount": "",
-        "manage_stock": "no",
-        "tax_status": "taxable",
-        "upsell_ids": "",
-        "crosssell_ids": "",
-        "purchase_note": "",
-        "sale_price_dates_from": "",
-        "sale_price_dates_to": "",
-        "download_limit": "0",
-        "download_expiry": "0",
-        "product_url": "",
-        "button_text": "",
-        "images": image,
-        "downloadable_files": "",
-        "product_page_url": "",
-        "meta:total_sales": "",
-        "tax:pwb-brand": "",
-        "tax:product_type": "",
-        "tax:product_visibility": "",
-        "tax:product_cat": category,
-        "tax:product_tag": tags,
-        "tax:product_shipping_class": ""
-    }
+    "post_title": name,
+    "post_name": slug,
+    "post_parent": "",
+    "ID": "",
+    "post_content": description_html,
+    "post_excerpt": summary,
+    "post_status": "publish",
+    "post_password": "",
+    "menu_order": "",
+    "post_date": "",
+    "post_author": "",
+    "comment_status": "open",
+    "_sku": sku,
+    "parent_sku": "",
+    "parent": "",
+    "_children": "",
+    "_downloadable": "no",
+    "_virtual": "no",
+    "_stock": "",
+    "_regular_price": price,
+    "_sale_price": "",
+    "_weight": "",
+    "_length": "",
+    "_width": "",
+    "_height": "",
+    "_tax_class": "",
+    "_visibility": "",
+    "_stock_status": "instock",
+    "_backorders": "no",
+    "_manage_stock": "no",
+    "_tax_status": "taxable",
+    "_upsell_ids": "",
+    "_crosssell_ids": "",
+    "_featured": "",
+    "_sale_price_dates_from": "",
+    "_sale_price_dates_to": "",
+    "_download_limit": "0",
+    "_download_expiry": "0",
+    "_product_url": "",
+    "_button_text": "",
+    "meta:_yoast_wpseo_focuskw": "",
+    "meta:_yoast_wpseo_title": "",
+    "meta:_yoast_wpseo_metadesc": "",
+    "meta:_yoast_wpseo_metakeywords": "",
+    "images": featured_images,
+    "downloadable_files": "",
+    "product_page_url": "",
+    "tax:product_type": "simple",
+    "tax:product_visibility": "",
+    "tax:product_cat": category,
+    "tax:product_tag": ', '.join(tags) if tags else None,
+    "tax:product_shipping_class": "",
+    "meta:partdo_post_views_count": "",
+    "meta:total_sales": "",
+    "attribute": attribute,
+    "attribute_data": attribute_data,
+    "attribute_default": attribute_default
+}
 
     # Append the product data to the list
     data.append(product_data)
 
 # Specify the file name for the CSV file
-csv_file_name = 'rhapsodybikeparts.csv'
+csv_file_name = 'rcfminibikes.csv'
 
 # Get the absolute path of the current working directory
 current_dir = os.getcwd()
@@ -230,18 +308,24 @@ csv_file_path = os.path.join(current_dir, csv_file_name)
 
 # Write the product data to a CSV file
 with open(csv_file_path, 'w', encoding='utf8', newline='') as f:
-    fc = csv.DictWriter(f, fieldnames=data[0].keys())
-    fc.writeheader()
+    # Check if the data list is not empty
+    if data:
+        fc = csv.DictWriter(f, fieldnames=data[0].keys())
+        fc.writeheader()
+        
+        print('Now writing data to CVS file...')
 
-    # Use tqdm to track the progress of writing
-    progress_bar = tqdm(total=len(data), desc='Writing CSV')
+        # Use tqdm to track the progress of writing
+        progress_bar = tqdm(total=len(data), desc='Writing CSV')
 
-    for row in data:
-        fc.writerow(row)
-        progress_bar.update(1)
+        for row in data:
+            fc.writerow(row)
+            progress_bar.update(1)
 
-    # Close the tqdm progress bar
-    progress_bar.close()
+        # Close the tqdm progress bar
+        progress_bar.close()
+    else:
+        print('No data available. CSV file not created.')
 
 # Open the CSV file after writing is done
 try:
